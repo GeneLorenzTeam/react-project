a54730bc22133fee767386390dbd25dcb8800262
genelorenz0408 <sarmientoemmener83@gmail.com>
2021-10-01 15:20:26 +0800

    Create 404 error

--- src/App.js
+++ src/App.js
@@ -2,49 +2,39 @@
 import Navbar from './Components/Navbar'
 import 'bootstrap/dist/css/bootstrap.css';
 import {
-  
+  Redirect,
   Switch,
   Route
 } from "react-router-dom";
-function App() {
+import Home from './Components/Navbar/Home';
+
+function App({ currentUser, checkUserSession, dsa }) {
   return (
       <div className="App">
         <Navbar/>
         <Switch>
-        {/* If the current URL is /about, this route is rendered
-            while the rest are ignored */}
+        <Route path="/" exact>
+          <Home />
+        </Route>
         <Route path="/about">
           <About />
         </Route>
-
-        {/* Note how these two routes a`re ordered. The more specific
-            path="/contact/:id" comes before path="/contact" so that
-            route will render when viewing an individual contact */}
-        
+      
         <Route path="/contact">
           <Users />
-        </Route>
-
-        {/* If none of the previous routes render anything,
-            this route acts as a fallback.
-
-            Important: A route with path="/" will *always* match
-            the URL because all URLs begin with a /. So that's
-            why we put this one last of all */}
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route render={function () {
-              return <p>Not found</p>
-            }} />
+        </Route> 
+        <Route path="/home">
+            <Home />
+          </Route>
+            <Route path="*">
+              <PageNotFound />
+            </Route>
+         
       </Switch>
       </div>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
 
 function About() {
   return <h2>About</h2>;  
@@ -52,5 +42,8 @@ function About() {
 
 function Users() {
   return <h2>Users</h2>;
+}
+function  PageNotFound(){
+  return <h3>dwsds</h3>
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './index.scss';
 import App from './App';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import Home from './Components/Navbar/Home';
 
 ReactDOM.render(
 <BrowserRouter>
